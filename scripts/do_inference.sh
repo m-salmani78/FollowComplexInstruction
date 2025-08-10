b12106b560a904761b16b0bcd10e1dc72d62fb72
m-salmani78 <bvlek822@gmail.com>
2025-08-10 14:33:51 +0000

    Update do_inference (Replace with vLLM)

--- scripts/do_inference.sh
+++ scripts/do_inference.sh
@@ -1,5 +1,9 @@
-CUDA_VISIBLE_DEVICES=YOUR_CUDA_DEVICES python ../get_data/do_inference.py \
-    --data_path=../get_data/data/data.jsonl \
-    --res_path=../get_data/data/res_llama2.jsonl \
-    --model_path=PATH_TO_YOUR_MODEL\
-    --lora_path=PATH_TO_YOUR_LORA_WEIGHT IF YOU USE LORA \
+python /teamspace/studios/this_studio/FollowComplexInstruction/get_data/do_inference.py \
+  --data_path=/teamspace/studios/this_studio/FollowComplexInstruction/get_data/data/data.jsonl \
+  --res_path=/teamspace/studios/this_studio/FollowComplexInstruction/get_data/data/res_gemma2_2b_it.jsonl \
+  --model_path google/gemma-2-2b-it \
+  --batch_size 32 \
+  --max_model_len 4096 \
+  --max_tokens 512 \
+  --gpu_memory_utilization 0.9 \
+  --dtype bfloat16
